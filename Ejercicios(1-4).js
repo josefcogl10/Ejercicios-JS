851c594b0efad65722d3f03c4b0435d8586fe800
josefcogl10 <li.b.ra@hotmail.com>
2025-08-26 12:19:07 -0600

    Ajustes de Ejericicios 1-4, Nuevos Ejercicios 5-8

--- Ejercicios(1-4).js
+++ Ejercicios(1-4).js
@@ -2,7 +2,6 @@
     Ejercicios de Lógica de Programación ( 1 / 10 )
 */
 
-
 // 1. Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10
 
 function contarCaracteres(cadena) {
@@ -17,39 +16,66 @@ contarCaracteres(texto);
 
 // 2. Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola"
 function recortarTexto(texto, valor) {
-    if (typeof texto === "string" && typeof valor === "number") {
-        return console.info(texto.slice(0, valor));
+    if (typeof texto === "string") {
+        if (typeof valor === "number") {
+            return console.info(texto.slice(0, valor));
+        } else {
+            return console.warn("Debe de ingresar el valor de caracteres a recortar");
+        }
     } else {
-        return console.warn("Debe de ingresar una cadena de texto y el valor de caracteres a recortar");
+        return console.warn("Debe de ingresar una cadena de texto");
     }
 }
 
 const texto2 = "Hola Dev´s";
-recortarTexto(texto2, 2);
+recortarTexto(texto2, 9);
 
 // 3. Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal']
 function separarTexto(texto, separador) {
-    if (typeof texto === "string" && typeof separador === "string") {
-        return console.info(texto.split(separador));
-    } else {
-        return console.warn("Debe de ingresar una cadena de texto y el caracter utilizado para separar el texto");
-    }
+    /**
+        if (typeof texto === "string" && typeof separador === "string") {
+            return console.info(texto.split(separador));
+        } else {
+            return console.warn("Debe de ingresar una cadena de texto y el caracter utilizado para separar el texto");
+        }
+    */
+
+    // Optimización del código utilizando concatenación de operadores ternarios
+    !(typeof texto === "string" && texto)
+        ? console.warn("Debe de ingresar una cadena de texto a separar")
+        : !(typeof separador === "string")
+            ? console.warn("Debe de ingresar el caracter utilizado para separar el texto")
+            : console.info(texto.split(separador));
 }
 
 const texto3 = "Texto utilizado para separar con algún valor";
-separarTexto(texto3, " ");
+separarTexto(texto3, "t");
 
 
 // 4. Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo
 
-function repetirTextoNveces(texto, veces) {
-    if (typeof texto === "string" && typeof veces === "number") {
+function repetirTextoNveces(texto = "", veces = undefined) {
+    /**
+     * 
+     *  if (typeof texto === "string" && typeof veces === "number") {
         texto = texto + ", ";
         return console.info(texto.repeat(veces))
-    } else {
-        return console.warn("Debe de ingresar una cadena de texto y el valor de veces a repetir el texto");
-    }
+        } else {
+            return console.warn("Debe de ingresar una cadena de texto y el valor de veces a repetir el texto");
+        }
+     * 
+     */
+
+    //texto = texto + ", ";
+    (!texto && typeof texto === "string")
+        ? console.warn("Debe de ingresar una cadena de texto a repetir")
+        : (veces === undefined && typeof veces !== "number")
+            ? console.warn("Debe de ingresar el valor de veces a repetir el texto")
+            : console.info(texto.repeat(veces));
 }
 
 const texto4 = "Me gusta el café";
-repetirTextoNveces(texto4, 2);
+repetirTextoNveces();
+repetirTextoNveces(texto4);
+repetirTextoNveces("", 2);
+repetirTextoNveces(texto, 2);
---
+++ Ejercicios(2-8).js
@@ -0,0 +1,88 @@
+/** Ejercicios de Lógica de Programación ( 2 / 10 ) */
+
+/** 5. Programa una función que invierta las palabras de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH" */
+function invertirCadena(cadena) {
+    let cadenaInvertida = "";
+
+    if (cadena && typeof cadena === "string") {
+        cadenaInvertida = cadena.split("").reverse()
+        console.info(cadenaInvertida.toString().replace(/,/g, ""));
+    } else {
+        return console.warn("Debes de ingresar una cadena de texto");
+    }
+}
+
+const cadena = "HOLA MUNDO";
+invertirCadena(cadena);
+
+/** 6. Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2 */
+function contarPalabraEncontradas(cadena, palabra) {
+    let contador = 0;
+    const txtLimpio = cadena.replace(/[.,;:!?()"]/g, ""); // elimina puntuación básica
+
+    (txtLimpio && palabra)
+        ? (typeof txtLimpio === "string")
+            ? (typeof palabra === "string")
+                ? //txtLimpio.split(" ").forEach((elemento) => {
+                //  if (elemento === palabra) { contador++; }
+                //})
+                contador = txtLimpio
+                    .split(/\s+/)
+                    .filter(elemento => elemento === palabra).length
+                : console.warn("No se encontro la palabra a buscar")
+            : console.warn("Debe de ingresar una cadena de texto")
+        : console.warn("Debe de ingresar una cadena de texto y la palabra a buscar");
+
+    return console.info(`La palabra ${palabra} se ha encontrado ${contador} veces en la cadena de texto`);
+}
+
+const cadena2 = "Hola mundo, adios mundo. El mundo es un lugar maravilloso para vivir en este mundo";
+const palabra = "mundo";
+contarPalabraEncontradas(cadena2, palabra);
+
+/** 7. Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true */
+function esPalindromo(cadena) {
+    if (!cadena) {
+        console.warn("Debe de ingresar una cadena de texto");
+        return;
+    }
+
+    if (typeof cadena !== "string") {
+        console.warn("La cadena debe ser de tipo string");
+        return;
+    }
+
+    const cadenaLimpia = cadena.toLowerCase().replace(/[\W_]/g, ''); // elimina espacios y caracteres no alfanuméricos
+    const cadenaInvertida = cadenaLimpia.split('').reverse().join('');
+    (cadenaLimpia === cadenaInvertida)
+        ? console.info(`La cadena "${cadena}" es un palíndromo`)
+        : console.info(`La cadena "${cadena}" no es un palíndromo`);
+}
+const cadenaPalindromo = "Salas";
+esPalindromo(cadenaPalindromo);
+
+/** 8. Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5 */
+function eliminarPatron(cadena, patron) {
+    if (!cadena || !patron) {
+        console.warn("Debe de ingresar una cadena de texto y un patron a eliminar");
+        return;
+    }
+
+    if (typeof cadena !== "string") {
+        console.warn("La cadena debe ser de tipo string");
+        return;
+    }
+
+    if (typeof patron !== "string") {
+        console.warn("El patron debe ser de tipo string");
+        return;
+    }
+
+    const cadenaLimpia = cadena.replace(new RegExp(patron, 'g'), '');
+    console.info(cadenaLimpia);
+
+}
+
+const cadena3 = "xyz1, xyz2, xyz3, xyz4 y xyz5";
+const patron = "xyz";
+eliminarPatron(cadena3, patron);
